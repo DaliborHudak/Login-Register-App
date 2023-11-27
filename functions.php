f45106f982bca669ebab80841ce60fcebd9ab34b
DaliborHudak <daliborhudak123456798@gmail.com>
2023-11-27 21:39:14 +0100

    fully working validation on user register updated with commented and cleaned code with redirect

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-// Funkce pro registraci nového uživatele
 function registerUser($conn, $username, $password, $email) {
     // Ověření existence e-mailu ve formuláři
     if (empty($email)) {
@@ -11,38 +10,36 @@ function registerUser($conn, $username, $password, $email) {
         return "All fields are required.";
     }
 
+    // Kontrola validity e-mailové adresy
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "Invalid e-mail format.";
+    }
+
     // SQL dotaz pro kontrolu, zda uživatel s daným jménem již existuje
     $check_existing_user_sql = "SELECT * FROM my_users WHERE username = '$username'";
     $existing_user_result = $conn->query($check_existing_user_sql);
 
-    // Ověření existence e-mailu v databázi
-    $check_existing_user_sql = "SELECT * FROM my_users WHERE username = '$username'";
-    $existing_user_result = $conn->query($check_existing_user_sql);
-
-    if ($existing_user_result->num_rows > 0) {
-        return "Not registered. User already exists.";
-    } else {
-        
-    }
-
+    // SQL dotaz pro kontrolu, zda e-mail již existuje v databázi
+    $check_existing_email_sql = "SELECT * FROM my_users WHERE email = '$email'";
+    $existing_email_result = $conn->query($check_existing_email_sql);
 
     // Pokud uživatel existuje, vrátíme chybovou zprávu
     if ($existing_user_result->num_rows > 0) {
         return "Not registered. User already exists.";
+    } elseif ($existing_email_result->num_rows > 0) {
+        return "Not registered. Email is already used.";
     } else {
         // Uživatel neexistuje, provedeme registraci
         $insert_user_sql = "INSERT INTO my_users (username, password, email) VALUES ('$username', '$password', '$email')";
         
-        // Pokud registrace proběhne úspěšně, přesměrujeme na jinou stránku
+        // Pokud registrace proběhne úspěšně, přesměrujeme na reg-redirect.php
         if ($conn->query($insert_user_sql) === TRUE) {
-            // Uživatel byl úspěšně zaregistrován, přesměrujte na jinou stránku
-            redirect("reg-redirect.php");
+            header("Location: reg-redirect.php");
+            exit(); // Zajištění, že se skript po přesměrování okamžitě ukončí
         } else {
             // Pokud dojde k chybě při registraci, vrátíme chybovou zprávu
             return "Error: " . $insert_user_sql . "<br>" . $conn->error;
         }
-
-
     }
 }
 
